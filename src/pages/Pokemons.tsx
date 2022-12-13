935cdd70b11d247a5963eca5a50a43bee7ed8145
Alex <alextr_r@outlook.com>
2022-12-13 19:59:45 -0300

    Add project

---
+++ src/pages/Pokemons.tsx
@@ -0,0 +1,62 @@
+import Header from "../components/Header";
+import { useState } from "react";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
+
+import BulbasaurPic from "../assets/bulbasaur.gif";
+import styles from "./pokemons.module.css";
+
+const Pokemons = () => {
+    const [query, setQuery] = useState("");
+    return (
+    <>
+        <Header query={query} setQuery={setQuery}/>
+
+            <main>
+                <nav>
+                    <Link className={styles.listItem} to="/">
+                        <img 
+                            className={styles.listItemIcon} 
+                            src={BulbasaurPic} 
+                            alt="bulbasaur" 
+                        />
+                        <div className={styles.listItemText}>
+                            <span>Bulbasaur</span>
+                            <span>001</span>
+                        </div>
+                    </Link>
+
+                    <Link className={styles.listItem} to="/">
+                        <img 
+                            className={styles.listItemIcon} 
+                            src={BulbasaurPic} 
+                            alt="bulbasaur" 
+                        />
+                        <div className={styles.listItemText}>
+                            <span>Bulbasaur</span>
+                            <span>001</span>
+                        </div>
+                    </Link>
+
+                    <Link className={styles.listItem} to="/">
+                        <img 
+                            className={styles.listItemIcon} 
+                            src={BulbasaurPic} 
+                            alt="bulbasaur" 
+                        />
+                        <div className={styles.listItemText}>
+                            <span>Bulbasaur</span>
+                            <span>001</span>
+                        </div>
+                    </Link>
+                    
+                </nav>
+            </main>
+
+        <Footer />
+
+    </>
+    );
+};
+
+export default Pokemons;
